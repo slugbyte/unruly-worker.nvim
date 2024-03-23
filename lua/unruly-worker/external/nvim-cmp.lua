90004bc79d244cd7e2f05b5441b919f4ffde794a
Duncan Marsh <slugbyte@slugbyte.com>
2024-03-23 13:10:22 +0800

    tweeking unruly keymaps and cmp presets

--- lua/unruly-worker/external/nvim-cmp.lua
+++ lua/unruly-worker/external/nvim-cmp.lua
@@ -2,40 +2,53 @@
 -- TODO: setup luasnip
 -- TODO: create a way to create a skip list?
 -- TODO: create a way to merge additional mappings?
+-- TODO: find a way not to directly depend on cmp?
+local util = require("unruly-worker.util")
+
 local function create_module(config)
 	if config == nil then
 		config = {}
 	end
 
-	local status, cmp = pcall(require, "cmp")
-	if (not status) or (cmp == nil) then
+	local cmp = config.cmp
+	if (cmp == nil) then
+		util.notify_error("cmp not found")
 		return nil
 	end
 
-	local types = require("cmp.types")
+	local luasnip = require("luasnip")
+	vim.print("luasnip", luasnip)
+	--
+	--
+	-- local select_behavior_insert = "insert"
+	-- local select_behavior_continue = "select"
 
 	local action_abort = cmp.mapping.abort()
 	local action_confirm_select = cmp.mapping.confirm({ select = true })
 	local action_confirm_continue = cmp.mapping.confirm({ select = false })
-	local action_insert_next = cmp.mapping.select_next_item({ behavior = types.cmp.SelectBehavior.Select })
-	local action_insert_prev = cmp.mapping.select_prev_item({ behavior = types.cmp.SelectBehavior.Select })
+	local action_insert_next = cmp.mapping.select_next_item({ behavior = "select" })
+	local action_insert_prev = cmp.mapping.select_prev_item({ behavior = "select" })
 
-	-- local action_luasnip_next = cmp.mapping(function()
-	-- 	if luasnip.expand_or_locally_jumpable() then
-	-- 		luasnip.jump(1)
-	-- 	end
-	-- end)
+	local action_luasnip_next = cmp.mapping(function()
+		if luasnip.expand_or_locally_jumpable() then
+			luasnip.jump(1)
+		end
+	end)
+
+	local action_luasnip_prev = cmp.mapping(function()
+		if luasnip.locally_jumpable(-1) then
+			luasnip.jump(-1)
+		end
+	end)
 	--
-	-- local action_luasnip_prev = cmp.mapping(function()
-	-- 	if luasnip.locally_jumpable(-1) then
-	-- 		luasnip.jump(-1)
-	-- 	end
-	-- end)
+	--
 
 	local default_insert_mapping = {
-		["<C-f>"] = { i = action_confirm_select },
-		["<C-j>"] = { i = action_confirm_continue },
+		["<C-f>"] = { i = action_confirm_continue },
+		["<C-j>"] = { i = action_confirm_select },
+
 		["<CR>"] = { i = action_confirm_select },
+		["<Right>"] = { i = action_confirm_select },
 
 		["<Tab>"] = { i = action_insert_next },
 		["<Down>"] = { i = action_insert_next },
@@ -44,14 +57,14 @@ local function create_module(config)
 		["<Up>"] = { i = action_insert_prev },
 
 		["<C-BS>"] = { i = action_abort },
-		-- ["<c-l>"] = {
-		-- 	i = action_luasnip_next,
-		-- 	s = action_luasnip_next,
-		-- },
-		-- ["<c-k"] = {
-		-- 	i = action_luasnip_prev,
-		-- 	s = action_luasnip_next,
-		-- },
+		["<c-l>"] = {
+			i = action_luasnip_next,
+			v = action_luasnip_next,
+		},
+		["<c-k"] = {
+			i = action_luasnip_prev,
+			v = action_luasnip_next,
+		},
 	}
 
 	local function action_cmdline_next()
@@ -74,7 +87,6 @@ local function create_module(config)
 		["<C-f>"] = { c = action_confirm_select },
 		["<C-j>"] = { c = action_confirm_continue },
 		["<Right>"] = { c = action_confirm_continue },
-		["<CR>"] = { c = action_confirm_continue },
 		["<Tab>"] = { c = action_cmdline_next },
 		["<S-Tab>"] = { c = action_cmdline_prev },
 	}
